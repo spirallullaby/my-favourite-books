9a68c301e25df3c573122b3c500ca16e2667a1a8
spirallullaby <ralicatst@gmail.com>
2021-01-03 14:24:26 +0200

    Add validations.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,25 +12,32 @@ class BooksController < ApplicationController
   def create
     params.require(:book)
     permitted = params[:book].permit(:title, :isbn, :genre, :description, :publish_date)
-    @book = Book.create!(permitted)
-    flash[:notice] = "#{@book.title} was successfully created."
-    redirect_to book_path
+    @book = Book.new(permitted)
+    if @book.save
+      flash[:notice] = "#{@book.title} was successfully created."
+      redirect_to books_path
+    else
+      render 'new'
+    end
+  end
+  def edit
+   @book = Book.find params[:id]
+  end
+  def update
+   @book = Book.find params[:id]
+   params.require(:book)
+   permitted = params[:book].permit(:title, :isbn, :genre, :description, :publish_date)
+   if @book.update(permitted)
+     flash[:notice] = "#{@book.title} was successfully updated."
+     redirect_to book_path(@book)
+   else
+     render 'edit'
    end
-   def edit
-    @book = Book.find params[:id]
-   end
-   def update
-    @book = Book.find params[:id]
-    params.require(:book)
-    permitted = params[:book].permit(:title, :isbn, :genre, :description, :publish_date)
-    @book.update_attributes!(permitted)
-    flash[:notice] = "#{@book.title} was successfully updated."
-    redirect_to books_path(@book)
-   end
-   def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    flash[:notice] = "Book '#{@book.title}' deleted."
-    redirect_to books_path
-   end
+  end
+  def destroy
+   @book = Book.find(params[:id])
+   @book.destroy
+   flash[:notice] = "Book '#{@book.title}' deleted."
+   redirect_to books_path
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,13 @@
-class Book < ApplicationRecord    
+class Book < ApplicationRecord  
+    def self.all_genres
+        @@all_genres = ["Science fiction", "Drama", "Action and Adventure", "Romance", "Mystery", "Horror"]
+    end
+    validates :title, :presence => true
+    validates :publish_date, :presence => true
+    validate :released_1967_or_later # uses custom validator below
+    validates :genre, :inclusion => {:in => Book.all_genres}
+    def released_1967_or_later
+        errors.add(:isbn, 'must be added if the book was released after 1967') if
+        publish_date > Date.parse('1 Jan 1967') && (isbn.nil? || isbn.empty?)
+    end
 end
